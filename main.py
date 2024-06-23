3d251e86194ce5118a6020278766ac44578e81f2
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-23 21:20:57 +0900

    organized, backed up and output log successfully

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ fmt = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO, format=fmt)
 # logging.info('Hello World.')
 
-# def output_log(type, msg):
+def output_log(log_type, msg):
+    print(msg)
+    if log_type == "info":
+        logging.info(msg)
+    elif log_type == "error":
+        logging.error(msg)
+    else:
+        logging.critical("Failed to output the log file.")
+
 #     fmt = "%(asctime)s - %(levelname)s - %(message)s"
 #     logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO, format=fmt)
 #     logging.info('Hello World.')
@@ -27,6 +35,8 @@ def get_backup(file_name, lines):
     f = open(f"{BACKUP_DIL}/bak-{datestr}-{file_name}.ini", "w")
     for line in lines:
         f.write(line)
+    # logging.info(f'Backed up "{file_name}".')
+    output_log("info", f'Backed up "{file_name}".')
     f.close()
 
 def delete_backup():
@@ -41,7 +51,8 @@ def delete_backup():
         num = files - BACKUP_MAX
         for i in range(num):
             os.remove(f'{BACKUP_DIL}/{file_names[i]}')
-            logging.info(f'Deleted "{file_names[i]}".')
+            # logging.info(f'Deleted "{file_names[i]}".')
+            output_log("info", f'Deleted "{file_names[i]}".')
 
 
 def get_header(lines):
@@ -52,28 +63,17 @@ def get_header(lines):
         else:
             return tempstr
 
-def organize_presets():
-    # print("Hello world!!!!!!!!!")
-    # test/vst3-bx_console Focusrite SC.ini
-    print("### read a file")
-    f_target_path = open("target_path.ini", "r")
-    f_target_files = open("target_files.ini", "r")
-    target_path = f_target_path.readlines()
-    target_files = f_target_files.readlines()
-    try:
-        with open(f"{target_path}{target_files}") as f:
-            print("ファイルが存在する")
-    except FileNotFoundError:
-        # print("ファイルが存在しません")
-        output_log("error", f"{target_path}{target_files} does not exists.")
-
-    f = open(f"{target_path}{target_files}", "r") # r = readonly
+def organize_presets(path, file_name):
+    f = open(f"{path}{file_name}", "r") # r = readonly
     # print(f.read()) # success
     lines = f.readlines()
+    get_backup(file_name, lines)
+    f.close()
+
     presets = {}
     tempstr = ""
     # print(lines[1])
-    f2 = open("test/test.txt", "w") # w = create or overwrite
+    f2 = open(f"{path}{file_name}", "w") # w = create or overwrite
     for line in lines:
         # tempstr = line.replace("[Preset", "[EditedPreset")
         obj_mutch = re.search(r'\[Preset\d+\]', line)
@@ -100,8 +100,36 @@ def organize_presets():
         f2.write(converted_preset)
         num += 1
 
-    f.close()
+    # f.close()
     f2.close()
+
+def main():
+    # print("Hello world!!!!!!!!!")
+    # test/vst3-bx_console Focusrite SC.ini
+    print("### read a file")
+    f_target_path = open("target_path.ini", "r")
+    f_target_files = open("target_files.ini", "r")
+    target_path = f_target_path.readlines()
+    target_files = f_target_files.readlines()
+    for file in target_files:
+        target = f"{target_path[0]}{file}"
+        # organize_presets(target_path[0], file)
+        # logging.info(f'Organized "{target}".')
+        try:
+            with open(target) as f:
+                # print("ファイルが存在する")
+                # print(f"target_path_0: {target_path[0]}")
+                organize_presets(target_path[0], file)
+                # logging.info(f'Organized "{target}".')
+                output_log("info", f'Organized "{target}".')
+
+        except FileNotFoundError:
+            # print("ファイルが存在しません")
+            # output_log("error", f"{target_path}{target_files} does not exists.")
+            # tempstr = f"{target} does not exists."
+            # print(tempstr)
+            # logging.error(tempstr)
+            output_log("error", f"{target} does not exists.")
 
 def test():
     f = open("test/test.txt", "r")
@@ -112,5 +140,6 @@ def test():
 # organize_presets()
 
 # output_log()
-test()
+# test()
 # delete_backup()
+main()
