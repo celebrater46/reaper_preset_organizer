fe853637a371c27f6b7f2a32e0b5a900bcc01efd
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-23 20:42:44 +0900

    get and delete backups successfully

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 import re
+import os
+import logging
+from datetime import datetime
+
+BACKUP_DIL = "backup"
+BACKUP_MAX = 50
+
+fmt = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO, format=fmt)
+# logging.info('Hello World.')
+
+# def output_log(type, msg):
+#     fmt = "%(asctime)s - %(levelname)s - %(message)s"
+#     logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO, format=fmt)
+#     logging.info('Hello World.')
+
+    # logging.debug('Debug')
+    # logging.info('Informarion')
+    # logging.warning('Warning')
+    # logging.error('Error')
+    # logging.critical('Critical')
+
+def get_backup(file_name, lines):
+    now = datetime.now()
+    datestr = now.strftime("%Y%m%d%H%M%S")
+    f = open(f"{BACKUP_DIL}/bak-{datestr}-{file_name}.ini", "w")
+    for line in lines:
+        f.write(line)
+    f.close()
+
+def delete_backup():
+    # dir_path = "mypackage21"
+    file_names = [
+        f for f in os.listdir(BACKUP_DIL) if os.path.isfile(os.path.join(BACKUP_DIL, f))
+    ]
+    print(file_names)
+
+    files = len(file_names)
+    if files > BACKUP_MAX:
+        num = files - BACKUP_MAX
+        for i in range(num):
+            os.remove(f'{BACKUP_DIL}/{file_names[i]}')
+            logging.info(f'Deleted "{file_names[i]}".')
+
 
 def get_header(lines):
     tempstr = ""
@@ -12,7 +56,18 @@ def organize_presets():
     # print("Hello world!!!!!!!!!")
     # test/vst3-bx_console Focusrite SC.ini
     print("### read a file")
-    f = open("test/vst3-bx_console Focusrite SC.ini", "r") # r = readonly
+    f_target_path = open("target_path.ini", "r")
+    f_target_files = open("target_files.ini", "r")
+    target_path = f_target_path.readlines()
+    target_files = f_target_files.readlines()
+    try:
+        with open(f"{target_path}{target_files}") as f:
+            print("ファイルが存在する")
+    except FileNotFoundError:
+        # print("ファイルが存在しません")
+        output_log("error", f"{target_path}{target_files} does not exists.")
+
+    f = open(f"{target_path}{target_files}", "r") # r = readonly
     # print(f.read()) # success
     lines = f.readlines()
     presets = {}
@@ -48,5 +103,14 @@ def organize_presets():
     f.close()
     f2.close()
 
+def test():
+    f = open("test/test.txt", "r")
+    lines = f.readlines()
+    get_backup("test", lines)
+    f.close()
 
-organize_presets()
+# organize_presets()
+
+# output_log()
+test()
+# delete_backup()
