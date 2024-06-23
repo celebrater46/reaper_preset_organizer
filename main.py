41c36f2845d605b71d3a289f4c7a9190a3c439a2
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-23 18:11:45 +0900

    created the dictionary of presets successfully

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ def organize_presets():
     f = open("test/vst3-bx_console Focusrite SC.ini", "r") # r = readonly
     # print(f.read()) # success
     lines = f.readlines()
+    presets = {}
+    tempstr = ""
     # print(lines[1])
     f2 = open("test/test.txt", "w") # w = create or overwrite
     for line in lines:
         # tempstr = line.replace("[Preset", "[EditedPreset")
         obj_mutch = re.search(r'\[Preset\d+\]', line)
-        tempstr = ""
         if obj_mutch:
-            tempstr = obj_mutch.group() + "\r" # Preset03
-            tempstr = tempstr.replace("Preset", "pre")
+            tempstr = obj_mutch.group() + "\n" # Preset03
+            # tempstr = tempstr.replace("Preset", "pre")
+        elif line.find("Name=") > -1:
+            preset_name = line.replace("Name=", "").replace("\n", "")
+            presets[preset_name] = tempstr + line
+            # tempstr = ""
         else:
-            tempstr = line
-        f2.write(tempstr) # success
+            tempstr = tempstr + line
+        # f2.write(tempstr) # success
 
     f.close()
     f2.close()
+
+    print(presets)
 
 organize_presets()
