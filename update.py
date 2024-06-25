510cf580c2d15ec422cd45f0509b2630f7f68433
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-25 20:05:11 +0900

    edited update.py

--- update.py
+++ update.py
@@ -2,23 +2,30 @@
 import logging
 from datetime import datetime
 
-fmt = "%(asctime)s: %(message)s"
+fmt = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO, format=fmt)
+
+def get_lines():
+    f = open("readme.txt", 'r', encoding='utf-8')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def update_note():
+    lines = get_lines()
+    f2 = open("readme.txt", 'w', encoding='utf-8')
+    for line in lines:
+        if line.find("バージョン情報") > -1:
+            f2.write(line)
+            now = datetime.now()
+            datestr = now.strftime("%Y-%m-%d %H:%M:%S")
+            allstr = f"{datestr} - v{update_ver}: {update_info}"
+            f2.write(f"{allstr}\n")
+            logging.info(f"Updated a release note [{allstr}]")
+        else:
+            f2.write(line)
+    f2.close()
 
 update_ver = input('What version is it?: ')
 update_info = input('What did you update?: ')
-
-f = open("readme.txt", 'r', encoding='utf-8')
-lines = f.readlines()
-f.close()
-
-f2 = open("readme.txt", 'w', encoding='utf-8')
-for line in lines:
-    if line.find("バージョン情報") > -1:
-        f2.write(line)
-        now = datetime.now()
-        datestr = now.strftime("%Y-%m-%d %H:%M:%S")
-        f2.write(f"{datestr} - v{update_ver}: {update_info}\n")
-    else:
-        f2.write(line)
-f2.close()
+update_note()
